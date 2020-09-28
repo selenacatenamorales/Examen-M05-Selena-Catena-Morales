5d8f1befcbe01d5e25fa4356fc98fd93fc9b5a1d
Usuario <Usuario@DESKTOP-DDMTHU6>
2020-09-28 20:29:42 +0200

    gmm - he creat la funció de fav_character dins del codi compartit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        function gmm_favcharacter(){
+            echo "Harleen Quinzel aka Harley Quinn";
+        }
+        ?>
+    </body>
+</html>
